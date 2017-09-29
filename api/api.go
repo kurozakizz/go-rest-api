4bf85ed6c09507c7fc309fb4eba1063321bb9a15
nuttawoot <nuttawoot.s@app-alive.com>
2017-09-29 23:51:02 +0700

    minimal api

--- api/api.go
+++ api/api.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"net/http"
-	"strconv"
-
-	"github.com/kurozakizz/go-api/calculator"
 )
 
 func response(rw http.ResponseWriter, r *http.Request) {
-	question := "1,2"
-	sum := calculator.Add(question)
-	sumText := strconv.Itoa(sum)
-	rw.Write([]byte(question + " = " + sumText))
+	rw.Write([]byte("hello"))
 }
 
 func fail(rw http.ResponseWriter, r *http.Request) {
